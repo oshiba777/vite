d06623b9377280c0264c674f12f985394c5f7c82
oshiba777 <stik_xi@.mail.ru>
2025-09-19 12:21:29 +0500

    second

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const card = document.querySelector('.card');//выбираем все элементы с классом card на странице
+
+
+    document.querySelector(".card").addEventListener('click', ()=>{
+      console.log(0)
+    card.classList.toggle('card-show')
+});
+
